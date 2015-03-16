f302768cdbaf6b3bfde690a99eb91cc9a24d4c8b
Sky <Becca4Eva@live.com>
2015-03-16 16:46:55 +0330

    added operations option

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Sepehr Mohaghegh">
     <title>{{ Lang::get('messages.title') }}</title>
     <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/bootstrap-theme.min.css') }}" rel="stylesheet">
