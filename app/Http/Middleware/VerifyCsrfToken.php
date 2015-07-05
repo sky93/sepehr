fe7d10ca1c8f4c120b3eac5bbb31e6627cf2b440
Sky <Becca4Eva@live.com>
2015-07-05 17:52:45 +0430

    Remove CSRF token for some routes
    Remove CSRF token for some routes like Payments because banks don't
    support tokens!

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,8 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        //
+        'buy',
+        'tools/status',
+        'downloads',
     ];
 }
